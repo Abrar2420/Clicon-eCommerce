14eaf9ce7d48c5b4a3f70676107194e1e403894d
Abrar242002 <abrar242002@gmail.com>
2026-01-02 18:02:04 +0600

    part of banner secton

--- src/js/home.js
+++ src/js/home.js
@@ -1,1 +1,8 @@
-console.log("hello");
+// storing the banner remove button
+const removeBanner = document.querySelector("#remove-banner");
+// storing the banner part
+const banner = document.querySelector("#banner");
+// appling dospaly none when remove banner btn is clicked
+removeBanner.addEventListener("click", () => {
+  banner.classList.add("hidden");
+});
